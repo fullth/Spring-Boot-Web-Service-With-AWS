782d5cc2eda020c63c9f21157051ca7ba52b8b1f
th <lth129@naver.com>
2022-02-09 00:35:38 +0900

    📝 게시물 CRUD 기능 작성

--- src/main/java/com/fullth/web/springboot/service/PostsService.java
+++ src/main/java/com/fullth/web/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.fullth.web.springboot.service;
 
 import com.fullth.web.springboot.domain.posts.Posts;
 import com.fullth.web.springboot.domain.posts.PostsRepository;
+import com.fullth.web.springboot.dto.PostsListResponseDto;
 import com.fullth.web.springboot.dto.PostsResponseDto;
 import com.fullth.web.springboot.dto.PostsSaveRequestDto;
 import com.fullth.web.springboot.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -44,5 +48,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 게시글이 없습니다. id= " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    /** 트랜잭션 범위는 유지하고, 조회 기능만 남겨두어 조회 속도 개선 */
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.finsAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
