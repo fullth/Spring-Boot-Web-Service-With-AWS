5dfacf30b0a24ba5308e961eb991d58fda91890a
th <lth129@naver.com>
2022-02-18 01:37:06 +0900

    🕹 EC2 DI 에러 테스트

--- src/main/java/com/fullth/web/springboot/config/JpaConfig.java
+++ src/main/java/com/fullth/web/springboot/config/JpaConfig.java
@@ -2,7 +2,9 @@ package com.fullth.web.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories("com.fullth.web.springboot.domain")
 @EnableJpaAuditing
 public class JpaConfig {}
