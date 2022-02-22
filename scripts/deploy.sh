b63668fcd96d63845015f9ffb37f53fc084ac0d7
th <lth129@naver.com>
2022-02-22 23:29:25 +0900

    📝 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME \
+nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-prod.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-prod-db.properties \
     -Dspring.profiles.active=prod \
-    $REPOSITORY/nohup.out 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
