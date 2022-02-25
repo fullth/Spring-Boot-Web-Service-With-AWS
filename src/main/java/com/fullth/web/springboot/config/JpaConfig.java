a4da800535b7c7bfc57d79ff0f7c8736985edec8
th <lth129@naver.com>
2022-02-26 01:14:18 +0900

    @EntityScan 추가 후 테스트

--- src/main/java/com/fullth/web/springboot/config/JpaConfig.java
+++ src/main/java/com/fullth/web/springboot/config/JpaConfig.java
@@ -1,10 +1,14 @@
 package com.fullth.web.springboot.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
-@EnableJpaRepositories("com.fullth.web.springboot.domain")
+@EntityScan(basePackages = {"com.fullth.web.springboot.domain"})
+@EnableJpaRepositories(basePackages = {"com.fullth.web.springboot.domain"})
 @EnableJpaAuditing
+@Configuration
 public class JpaConfig {}
+
+
