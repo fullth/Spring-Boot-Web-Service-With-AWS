ce005e147cb8db56e0652c382192522f06e4afb8
th <lth129@naver.com>
2022-03-05 14:03:27 +0900

    PostService가 PostRepository의 주입을 못받음

--- src/main/java/com/fullth/web/springboot/config/JpaConfig.java
+++ src/main/java/com/fullth/web/springboot/config/JpaConfig.java
@@ -2,7 +2,9 @@ package com.fullth.web.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.fullth.web.springboot.domain")
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {}
