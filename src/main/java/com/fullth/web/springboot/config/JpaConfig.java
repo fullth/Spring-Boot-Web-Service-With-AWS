6ebbdf778a5b0a838edbb9ec19da6de108a3d195
th <lth129@naver.com>
2022-03-05 13:07:35 +0900

    aws code deploy 테스트

--- src/main/java/com/fullth/web/springboot/config/JpaConfig.java
+++ src/main/java/com/fullth/web/springboot/config/JpaConfig.java
@@ -1,12 +1,8 @@
 package com.fullth.web.springboot.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"com.fullth.web.springboot.domain"})
-@EnableJpaRepositories(basePackages = {"com.fullth.web.springboot.domain"})
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {}
