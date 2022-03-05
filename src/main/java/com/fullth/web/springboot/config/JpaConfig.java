81a63df25124c4f0a68a8f7cb8d46028463168ab
th <lth129@naver.com>
2022-03-05 14:24:59 +0900

    패키지 구조문제인지 확인

--- src/main/java/com/fullth/web/springboot/config/JpaConfig.java
+++ src/main/java/com/fullth/web/springboot/config/JpaConfig.java
@@ -1,9 +1,11 @@
 package com.fullth.web.springboot.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = "com.fullth.web.springboot.domain")
 @EnableJpaRepositories(basePackages = "com.fullth.web.springboot.domain")
 @EnableJpaAuditing
 @Configuration
